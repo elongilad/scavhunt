f1640eb23812e91af7efc60fa36c6ed76d0ba215
elongilad <149860142+elongilad@users.noreply.github.com>
2023-11-04 08:23:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("password-form").addEventListener("submit", function (ev
     event.preventDefault();
 
     var password = document.getElementById("password").value;
-    if (password === "mysecretpassword") {
+    if (password === "amosandeli") {
         document.getElementById("message").style.display = "block";
-        document.getElementById("message").innerHTML = "Welcome! Your secret text goes here.";
+        document.getElementById("message").innerHTML = "Welcome spies! Find the secret code in the storage unit!";
     } else {
         document.getElementById("message").style.display = "block";
         document.getElementById("message").innerHTML = "Incorrect password. Please try again.";
